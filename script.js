3c0e5ddd06f180de2bb61611b081ff3c177c7150
IvannaCheban <ivanna_cheban@ukr.net>
2023-06-02 19:06:46 +0300

    add close accout funtionality

--- script.js
+++ script.js
@@ -131,6 +131,7 @@ btnLogin.addEventListener("click", function (e) {
   currentAccount = accounts.find(
     (acc) => acc.username === inputLoginUsername.value
   );
+
   console.log(currentAccount);
   if (currentAccount?.pin === Number(inputLoginPin.value)) {
     //display UI and the message
@@ -169,4 +170,23 @@ btnTransfer.addEventListener("click", function (e) {
     //updating UI
     updateUI(currentAccount);
   }
+});
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    console.log(index);
+    //Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputClosePin.value = inputCloseUsername = ""; //clearing fields
 });
